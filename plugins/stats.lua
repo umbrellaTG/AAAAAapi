fc30e615e2c4dc64295e800f76a9358377cfe59e
umbrellaTG <abbasgh912@gmail.com>
2016-05-30 21:49:37 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'moon' then -- Put everything you like :)
+  if matches[1]:lower() == 'extreme' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "moon" then -- Put everything you like :)
+    if matches[2] == "extreme" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,13 +143,13 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (moon)",
-	"^[#!/]([Mm]oon)",
+    "^[#!/]([Ss]tats) (extreme)",
+	"^[#!/]([Ee]xtreme)",
     "^([Ss]tats)$",
     "^([Ss]tatslist)$",
     "^([Ss]tats) (group) (%d+)",
-    "^([Ss]tats) (telemoon)",
-	"^([Tt]elemoon)"
+    "^([Ss]tats) (extreme)",
+	"^([Ee]xtreme)"
     }, 
   run = run
 }
