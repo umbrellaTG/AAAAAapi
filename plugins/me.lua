d7894e12554b9733afa1c4abc3b4bae3dc53abad
umbrellaTG <abbasgh912@gmail.com>
2016-05-30 21:06:40 +0430

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -1,47 +1,31 @@
 do
-local TIME_CHECK = 60
-local function pre_process(msg)
-  local hash = 'per_sec'
-  local get = tonumber(redis:get(hash) or 0)
-  redis:setex(hash, TIME_CHECK, get)
-  return msg
-end
+
 local function run(msg, matches)
-  if matches[1] == "me" then
-    if msg.to.type == "user" then
-      return msg
+  if matches[1] == 'me' then
+    if is_sudo(msg) then
+    send_document(get_receiver(msg), "./plugins/sudo.webp", ok_cb, false)
+      return "SUDO"
+    elseif is_admin1(msg) then
+    send_document(get_receiver(msg), "./plugins/admin.webp", ok_cb, false)
+      return "You are My ADMIN"
+    elseif is_owner(msg) then
+    send_document(get_receiver(msg), "./plugins/owner.webp", ok_cb, false)
+      return "You are Group Owner"
+    elseif is_momod(msg) then
+    send_document(get_receiver(msg), "./plugins/mod.webp", ok_cb, false)
+      return "You are Group Moderator"
+    else
+    send_document(get_receiver(msg), "./plugins/member.webp", ok_cb, false)
+      return "You are Group Member"
     end
-    local hash = 'msgs:'..msg.from.id..':'..msg.to.id
-	local msgsend = redis:get(hash)
-	local r = tonumber(redis:get('cis_msg:'..msg.to.id))
-	local t = r * 100
-	local percent = math.floor((msgsend) / r * 100)
-	return reply_msg(msg.id, msg.from.print_name.." You've sent "..msgsend.."("..percent.."%) and this group has "..r.." messages", ok_cb, true)
   end
-  if matches[1] == "botload" and is_admin(msg) then
-    local hash = 'total:msg'
-    local get = tonumber(redis:get(hash))
-    local per_sec = tonumber(redis:get('per_sec'))
-    return per_sec.." msgs/s\n"..get.." Message since last restart"
-  end
-  if matches[1] == "restart" and is_sudo(msg) then
-    local hash = 'total:msg'
-    redis:del(hash)
-    return "See ya later =)"
-  end 
 end
 
 return {
   patterns = {
-    "^[/!#](me)$",
-    "^[/!#](botload)$",
-    "^[/!#](restart)$",
-    "^(me)$",
-    "^(botload)$",
-    "^(restart)$",
-  },
-  run = run,
-  pre_process = pre_process
+    "^[!/]([Mm]e)$",
+    "^([Mm]e)$"
+    },
+  run = run
 }
-
 end
